1e4c1c41f9c42094e45839a57e1d604581607052
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2021-05-27 13:00:07 +0530

    format user and stress-check

--- stress-check.py
+++ stress-check.py
@@ -1,9 +1,9 @@
-'''
+"""
 PREREQ:
 1) Attached Makefile 'stress' command
 2) User configuration set in 'user.py'
 3) Actual solution should be in 'sols/main.cpp' and brute should be in 'sols/brute.cpp'
-'''
+"""
 
 import os
 from subprocess import run, PIPE
@@ -13,64 +13,70 @@ from user import user_config, gen_case, checker
 
 # failed a test case
 def fail(tc, my_response, brute_response):
-	print("\n-- Test Case --\n")
-	print(*tc, sep = '\n')
-	print("\n-- My Output --\n")
-	print(*my_response, sep = '\n')
-	print("\n-- Brute Output --\n")
-	print(*brute_response, sep = '\n')
+    print("\n-- Test Case --\n")
+    print(*tc, sep="\n")
+    print("\n-- My Output --\n")
+    print(*my_response, sep="\n")
+    print("\n-- Brute Output --\n")
+    print(*brute_response, sep="\n")
 
-	if user_config["saveTC"] is not None:
-		with open(user_config["inputPipe"], "r") as f1:
-			lines = f1.readlines()
-			with open(user_config["saveTC"], "w") as f2:
-				f2.writelines(lines)
-		print(colored(f"\n> Test case saved at {user_config['saveTC']}", 'yellow'))
-			 
-	exit(0)	
+    if user_config["saveTC"] is not None:
+        with open(user_config["inputPipe"], "r") as f1:
+            lines = f1.readlines()
+            with open(user_config["saveTC"], "w") as f2:
+                f2.writelines(lines)
+        print(colored(f"\n> Test case saved at {user_config['saveTC']}", "yellow"))
+
+    exit(0)
 
 
 # TESTING LOOP =============>
-print(colored("\n#### Stress Testing .. ####", 'cyan', attrs = ['bold']))
+print(colored("\n#### Stress Testing .. ####", "cyan", attrs=["bold"]))
 for currentTc in range(1, user_config["testCases"] + 1):
 
-	# fetching test case
-	tc = gen_case()
-	if user_config["usedtt"]:
-		tc = ["1"] + tc
-	if user_config["debug"]:
-		print("Test case")
-		print(*tc, sep = '\n')
+    # fetching test case
+    tc = gen_case()
+    if user_config["usedtt"]:
+        tc = ["1"] + tc
+    if user_config["debug"]:
+        print("Test case")
+        print(*tc, sep="\n")
 
-	# write to inputPipe
-	stressIn = open(user_config["inputPipe"], "w")
-	for tline in tc:
-		stressIn.write(tline + "\n")
-	stressIn.close()
-	
-	# get my out
-	my_proc = run(["temp/myOut"], stdout = PIPE, stdin = open(user_config["inputPipe"], "r"))
-	my_response = (my_proc.stdout.decode('utf-8')).split('\n')
-	if user_config["removeBlanks"]:
-		my_response = [line for line in my_response if line.strip()] # removing blanks
-	if user_config["debug"]:
-		print("My response")
-		print(my_response) 
+    # write to inputPipe
+    stressIn = open(user_config["inputPipe"], "w")
+    for tline in tc:
+        stressIn.write(tline + "\n")
+    stressIn.close()
 
-	# get brute out
-	brute_proc = run(["temp/bruteOut"], stdout = PIPE, stdin = open(user_config["inputPipe"], "r"))
-	brute_response = (brute_proc.stdout.decode('utf-8')).split('\n')
-	if user_config["removeBlanks"]:
-		brute_response = [line for line in brute_response if line.strip()] # removing blanks
-	if user_config["debug"]:
-		print("Brute response")
-		print (brute_response)
+    # get my out
+    my_proc = run(
+        ["temp/myOut"], stdout=PIPE, stdin=open(user_config["inputPipe"], "r")
+    )
+    my_response = (my_proc.stdout.decode("utf-8")).split("\n")
+    if user_config["removeBlanks"]:
+        my_response = [line for line in my_response if line.strip()]  # removing blanks
+    if user_config["debug"]:
+        print("My response")
+        print(my_response)
 
-	# check if output matches
-	if not checker(my_response, brute_response = brute_response, inp = tc):
-		fail(tc, my_response, brute_response)
-	elif user_config["showEachPass"]: # test case passed
-		print (colored(".Passed case " + str(currentTc), 'green'))
+    # get brute out
+    brute_proc = run(
+        ["temp/bruteOut"], stdout=PIPE, stdin=open(user_config["inputPipe"], "r")
+    )
+    brute_response = (brute_proc.stdout.decode("utf-8")).split("\n")
+    if user_config["removeBlanks"]:
+        brute_response = [
+            line for line in brute_response if line.strip()
+        ]  # removing blanks
+    if user_config["debug"]:
+        print("Brute response")
+        print(brute_response)
+
+    # check if output matches
+    if not checker(my_response, brute_response=brute_response, inp=tc):
+        fail(tc, my_response, brute_response)
+    elif user_config["showEachPass"]:  # test case passed
+        print(colored(".Passed case " + str(currentTc), "green"))
 
 # success
-print(colored("\n> ALL BRUTE PASSED", 'cyan', attrs = ['bold']))
+print(colored("\n> ALL BRUTE PASSED", "cyan", attrs=["bold"]))
--- user.py
+++ user.py
@@ -3,7 +3,7 @@ from termcolor import colored
 import random
 
 
-'''
+"""
 user_config params:-
 
 testCases: Number of testCases to test against
@@ -14,42 +14,44 @@ whereFail: print where the output doesn't match
 inputPipe: input file used to feed input to executables
 debug: for each input -> print input, your output & brute output
 saveTC: path to a file which stores the failed test case, None if file is not to be saved
-'''
+"""
 user_config = {
-    "testCases" : 1000, 
-    "usedtt" : False,
-    "showEachPass" : True,
-    "removeBlanks" : True, 
-    "whereFail" : True, 
-    "inputPipe" : "temp/inputf.in",
-	"debug": False,
-	"saveTC": "sols/in.txt"
+    "testCases": 1000,
+    "usedtt": False,
+    "showEachPass": True,
+    "removeBlanks": True,
+    "whereFail": True,
+    "inputPipe": "temp/inputf.in",
+    "debug": False,
+    "saveTC": "sols/in.txt",
 }
 
 
 # ===========
 
 
-'''
+"""
 docs: 
 Generate your random test cases in this function
 
 return: 
 list of strings where each string is supposed to be displaed on a new line
-'''
+"""
+
+
 def gen_case():
-	lim = int(10)
-	dlim = 10
-	n = random.randint(1, lim)
-	a = [random.randint(-dlim, dlim) for i in range(n)]
-	tc = [str(n), ' '.join(list(map(str, a)))]
-	return tc
+    lim = int(10)
+    dlim = 10
+    n = random.randint(1, lim)
+    a = [random.randint(-dlim, dlim) for i in range(n)]
+    tc = [str(n), " ".join(list(map(str, a)))]
+    return tc
 
 
 # ===========
 
 
-'''
+"""
 docs: 
 Checks if your solution is correct based on an input
 Currently performs string comparison
@@ -61,23 +63,25 @@ my_response: list of strings denoting my output
 
 return: 
 boolean denoting if output satisifes the condition
-'''
-def checker(my_response, brute_response = None, inp = None):
+"""
+
+
+def checker(my_response, brute_response=None, inp=None):
     # check matching length
-	if len(my_response) != len(brute_response):
-		print(colored("\n!! FAIL !!", 'red', attrs = ['bold']))
-		if user_config["whereFail"]:
-			print("whereFail: UNMATCHING LENGTH")
-		return False
-		
-	# compare each line of response
-	N = len(my_response)
-	for i in range(N):
-		if my_response[i] != brute_response[i]:
-			print(colored("\n!! FAIL !!", 'red', attrs = ['bold']))
-			if user_config["whereFail"]:
-				print("whereFail: LINE", i + 1)
-				print("my_response: ", my_response[i])
-				print("brute_response: ", brute_response[i])
-			return False
-	return True
+    if len(my_response) != len(brute_response):
+        print(colored("\n!! FAIL !!", "red", attrs=["bold"]))
+        if user_config["whereFail"]:
+            print("whereFail: UNMATCHING LENGTH")
+        return False
+
+    # compare each line of response
+    N = len(my_response)
+    for i in range(N):
+        if my_response[i] != brute_response[i]:
+            print(colored("\n!! FAIL !!", "red", attrs=["bold"]))
+            if user_config["whereFail"]:
+                print("whereFail: LINE", i + 1)
+                print("my_response: ", my_response[i])
+                print("brute_response: ", brute_response[i])
+            return False
+    return True
